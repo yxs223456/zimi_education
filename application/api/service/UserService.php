fd33d46eed8ef309756a1c58e254085085caa965
yangxiushan <501942821@qq.com>
2020-03-23 12:07:58 +0800

    新手测试题结构修改

--- application/api/service/UserService.php
+++ application/api/service/UserService.php
@@ -667,6 +667,7 @@ class UserService extends Base
             "city" => $userInfo["city"],
             "bind_wechat" => empty($userInfo["mobile_openid"]) ? UserIsBindWeChatEnum::NO : UserIsBindWeChatEnum::YES,
             "novice_test_is_show" => $userInfo["novice_test_is_show"],
+            "novice_level" => $userInfo["novice_level"],
         ];
     }
 
